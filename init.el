d9849337e9f5a7abef41ea9c6660f73610f473ce
Yusuke Ujitoko <yusuke.ujitoko@gmail.com>
2016-11-20 10:17:21 +0900

    odify init.el

--- init.el
+++ init.el
@@ -8,21 +8,25 @@
 	    (normal-top-level-add-subdirs-to-load-path))))))
 
 ;;load-pathに追加するフォルダ
-;;2つ以上フォルダを指定する場合の引数 => (add-to-load-path "elisp" "xxx" "xxx")
-(add-to-load-path "elisp" "conf" "public_repos")
-;;(add-to-list 'load-path "elisp")
+(add-to-load-path "elisp" "elpa")
+
+(require 'package)
+;; MELPAを追加
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+;; MELPA-stableを追加
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+;; Marmaladeを追加
+(add-to-list 'package-archives  '("marmalade" . "http://marmalade-repo.org/packages/") t)
+;; Orgを追加
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+;; 初期化
+(package-initialize)
 
 ;;パッケージのインストールを自動化
 (when (require 'auto-install nil t)
   (setq auto-install-directory "~/.emacs.d/elisp/")
   (auto-install-update-emacswiki-package-name t)
   (auto-install-compatibility-setup))
-
-;;
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
-(fset 'package-desc-vers 'package--ac-desc-version)
-(package-initialize)
 
 ;;環境を日本語にする
 (require 'mozc)
@@ -58,16 +62,13 @@
 (fset 'yes-or-no-p 'y-or-n-p)
 
 ;; ;;フレームの透明度 意味ない？
-(set-frame-parameter (selected-frame) 'alpha '(0.85))
-
-;;カラーテーマ
-;;(load-theme 'adwaita')
-
-;;(cond (window-system
-;;       (setq x-select-enable-clipboard t)
-;;       ))
+(set-frame-parameter (selected-frame) 'alpha '(1.0))
 
 (setq x-select-enable-clipboard t)
+
+;; unvisualize menubar, toolbar
+(menu-bar-mode 0)
+(tool-bar-mode 0)
 
 ;;文字自体をコメントアウト
 (defun one-line-comment ()
@@ -79,6 +80,49 @@
     (comment-or-uncomment-region (region-beginning) (region-end))))
 
 (global-set-key (kbd "M-;") 'one-line-comment)
+
+
+;; Auto Complete
+(require 'auto-complete-config)
+(ac-config-default)
+(setq ac-use-menu-map t)       ;; 補完メニュー表示時にC-n/C-pで補完候補選択
+(setq ac-use-fuzzy t)          ;; 曖昧マッチ
+;;(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+;; '(custom-enabled-themes nil)
+;; '(custom-safe-themes
+;;   (quote
+;;    ("0c29db826418061b40564e3351194a3d4a125d182c6ee5178c237a7364f0ff12" default))))
+;;(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+;; )
+
+
+(load-theme 'spolsky t)
+
+
+(autoload 'markdown-mode "markdown-mode"
+   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+(autoload 'gfm-mode "gfm-mode"
+   "Major mode for editing GitHub Flavored Markdown files" t)
+(add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
+
+;; "C-t"でウィンドウ切り替える。初期値はtranspose-chars
+(defun other-window-or-split ()
+  (interactive)
+  (when (one-window-p)
+    (split-window-horizontally))
+  (other-window 1))
+(global-set-key (kbd "C-t") 'other-window-or-split)
 
 
 ;;companyの設定
@@ -95,6 +139,3 @@
 (add-hook 'c-mode-hook 'irony-mode)
 (add-hook 'c++-mode-hook 'irony-mode)
 (add-hook 'objc-mode-hook 'irony-mode)
-
-;; "C-t"でウィンドウ切り替える。初期値はtranspose-chars
-(define-key global-map (kbd "C-t") 'other-window)
